b810fcc2d38f4a1640439dda28571401c8100a4f
Menghok-72 <pichmenghok@gmail.com>
2023-02-18 10:35:42 +0700

    update

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import { ArticleContainer } from './Article';
-
-function App() {
+import { ArticleContainer } from './Components/Article';
+import NavTop from './Components/Navbar';
+ function App() {
   return (
     <div className="App">
-      <ArticleContainer/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      
+    <NavTop/>
+    <ArticleContainer/>
+    <h1>Hello</h1>
     </div>
   );
 }
-
 export default App;
+
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo "Welcome to  we Team";
-    echo "Let start us project";
-    echo "we can do it";
-
-?>c
