ebc2e6ebe15734c4ed13a0d648b17b83b973d778
Menghok-72 <pichmenghok@gmail.com>
2023-02-20 15:11:18 +0700

    update

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,11 +1,15 @@
+
 import { ArticleContainer } from './Components/Article';
 import NavTop from './Components/Navbar';
+import { Slide } from './Components/SlideShow';
  function App() {
   return (
     <div className="App">
     <NavTop/>
+    <Slide/>
     <ArticleContainer/>
     <h1>hello</h1>
+
     </div>
   );
 }
